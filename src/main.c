d118174f7ff5a048e1056d22fa3d319ad1f87fbb
Jonathan Eanes <jonathan.eanes@bsisentry.com>
2025-06-17 13:52:14 -0400

    add complex filename parsing for saving

--- src/main.c
+++ src/main.c
@@ -11,7 +11,52 @@
 #include "keybinds.h"
 
 GtkTextBuffer *buffer;
-char gblfilename[32];
+char gblfilename[PATH_MAX];
+char gblfilepath[PATH_MAX];
+
+/**
+ * Parses the given note title and generates safe values for gblfilename and gblfilepath.
+ *
+ * gblfilename will contain the filename (with .txt extension) relative to ~/Documents/GsTicKy.
+ * gblfilepath will contain the full absolute path to the file.
+ *
+ * @param title: the note title to be sanitized and used in the filename
+ * @return 1 on success
+ */
+int parse_title(const char *title)
+{
+    const char *home = getenv("HOME");
+    char safe_title[PATH_MAX];
+    int j = 0;
+    for (int i = 0; title[i] != '\0' && j < PATH_MAX - 1; i++)
+    {
+        if (g_ascii_isalnum(title[i]) || title[i] == '_' || title[i] == '-' || title[i] == '/') // now allow slashes
+        {
+            safe_title[j++] = title[i];
+        }
+        else
+        {
+            safe_title[j++] = '_';
+        }
+        safe_title[j] = '\0';
+    }
+    // gblfilename: relative path (filename only)
+    snprintf(gblfilename, PATH_MAX + sizeof(".txt"), "%s.txt", safe_title);
+    // gblfilepath: full path
+    snprintf(gblfilepath, PATH_MAX + sizeof(home) + sizeof("/Documents/GsTicKy/"), "%s/Documents/GsTicKy/%s", home, gblfilename);
+
+    // Now, try to get directories to work.
+    char dirpath[PATH_MAX]; // path through the directories based off of ~/Documents/GsTicKy
+    strncpy(dirpath, gblfilepath, PATH_MAX);
+    char *last_slash = strrchr(dirpath, '/');
+    if (last_slash)
+    {
+        *last_slash = '\0';
+        g_mkdir_with_parents(dirpath, 0755);
+    }
+
+    return 1;
+}
 
 /**
  * @return 1 on success, 0 on failure
@@ -21,36 +66,34 @@ int save_buffer()
     GtkTextIter start, end;
     gtk_text_buffer_get_bounds(buffer, &start, &end);
     char *text = gtk_text_buffer_get_text(buffer, &start, &end, true); // true on hidden chars to split by line
-
-    // process the char array to get the first line, however i know very little about string processing in C
     char *text_copy = g_strdup(text);
     char *token = strtok(text_copy, "\n");
     if (!token)
     {
         g_print("no token\n");
+        g_free(text_copy);
+        g_free(text);
         return 0;
     }
 
-    int title_len = strlen(token);
-    char title[32];
-    snprintf(title, sizeof(title), "%s", token);
-    // title now contains what we're naming the file
-    strcpy(gblfilename, title);
-    char filepath[PATH_MAX];
-    snprintf(filepath, sizeof(filepath), "%s/Documents/GsTicKy/%s.txt", getenv("HOME"), title);
-
-    FILE *fp = fopen(filepath, "w");
+    char title[PATH_MAX];
+    snprintf(title, PATH_MAX, "%s", token);
+    // Set gblfilename and gblfilepath
+    parse_title(title);
+    FILE *fp = fopen(gblfilepath, "w");
     if (fp)
     {
         fputs(text, fp);
         fclose(fp);
         g_free(text);
+        g_free(text_copy);
         return 1;
     }
     else
     {
         perror("Failed to write the file");
         g_free(text);
+        g_free(text_copy);
         return 0;
     }
 }
@@ -58,7 +101,7 @@ int save_buffer()
 static int save_cb(GtkWidget *widget, GVariant *args, gpointer user_data)
 {
     int status = save_buffer();
-    char *title = g_strdup_printf("GsTicKy - %s.txt", gblfilename);
+    char *title = g_strdup_printf("GsTicKy - %s", gblfilename);
     if (status)
     {
         gtk_window_set_title(GTK_WINDOW(widget), title);
@@ -82,12 +125,13 @@ static int delete_cb(GtkWidget *widget, GVariant *args, gpointer user_data)
     gtk_text_buffer_set_text(buffer, "", 0);
 
     // remove the file associated with the buffer, and clear the file
-    char filepath[PATH_MAX];
-    snprintf(filepath, sizeof(filepath), "%s/Documents/GsTicKy/%s.txt", getenv("HOME"), gblfilename);
-    if (remove(filepath) == 0) {
+    if (remove(gblfilepath) == 0)
+    {
         gtk_window_set_title(GTK_WINDOW(widget), "GsTicKy");
         return 1;
-    } else {
+    }
+    else
+    {
         return 0;
     }
 }
@@ -127,11 +171,11 @@ void set_always_on_top(GtkWindow *window)
     GtkNative *native = gtk_widget_get_native(GTK_WIDGET(window));
     GdkSurface *surface = gtk_native_get_surface(native);
 
-    #pragma GCC diagnostic push
-    #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
     Display *xdisplay = GDK_DISPLAY_XDISPLAY(display);
     Window xwindow = GDK_SURFACE_XID(surface);
-    #pragma GCC diagnostic pop
+#pragma GCC diagnostic pop
 
     Atom wm_state = XInternAtom(xdisplay, "_NET_WM_STATE", False);
     Atom wm_state_above = XInternAtom(xdisplay, "_NET_WM_STATE_ABOVE", False);
@@ -191,7 +235,7 @@ static void activate(GtkApplication *app, gpointer user_data)
 
     GtkEventController *delete_ctrl = gtk_shortcut_controller_new(); // deletes the note's contents and the file record of it
     create_keybind(GTK_WIDGET(window), delete_ctrl, delete_cb, GDK_KEY_x, GDK_CONTROL_MASK | GDK_SHIFT_MASK);
-   
+
     GtkEventController *quit_ctrl = gtk_shortcut_controller_new(); // quits the program and saves the note with the contents of the first line as the title
     create_keybind(GTK_WIDGET(window), quit_ctrl, quit_cb, GDK_KEY_d, GDK_CONTROL_MASK);
 
